4fc46ab11a8ce5bf7eb0376f7aa089fa6d3d4172
Roberto Pablo Carreno Zambrano <r.carrenoz@uniandes.edu.co>
2022-10-29 22:30:03 -0500

    feat: airport.module.ts

--- src/airport/airport.module.ts
+++ src/airport/airport.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AirportService } from './airport.service';
 import { AirportController } from './airport.controller';
+import { AirportEntity } from './airport.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AirportEntity])],
   providers: [AirportService],
-  controllers: [AirportController]
+  controllers: [AirportController],
 })
 export class AirportModule {}
